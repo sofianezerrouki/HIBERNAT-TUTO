156d59df93c854f3f04162121bece055cc01bf20
sofiane <sofianeyooung@gmail.com>
2020-10-14 23:27:14 +0100

    query a list of students

---
+++ src/com/camacode/hibernat/demo/QueryStudentDemo.java
@@ -0,0 +1,51 @@
+package com.camacode.hibernat.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.camacode.hibernat.demo.entity.Student;
+
+public class QueryStudentDemo {
+
+	public static void main(String[] args) {
+		
+		//create a session factory
+		SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Student.class)
+										.buildSessionFactory();
+		
+		//create a session 
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start a transaction 
+			System.out.println("Begin transaction .... ");
+			session.beginTransaction();
+			
+			//query students
+			List<Student> students = session.createQuery("from Student")
+									.getResultList();
+			
+			//display the students
+			for (Student student : students) {
+				System.out.println(student);
+			}
+			
+			//commit transaction
+			session.getTransaction().commit();
+			System.out.println("Done !");
+
+
+		} 
+		finally {
+			factory.close();
+		}
+		
+
+	}
+
+}
